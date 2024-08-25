12de2e4904b63405052508c891b215d056962c18
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-08-26 01:46:15 +0200

    Fix compilation with recent Microsoft UCRT headers
    Where the NAN macro now expands to a function call unless
    _UCRT_NOISY_NAN is defined.

--- quickjs.c
+++ quickjs.c
@@ -22,6 +22,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#ifdef _MSC_VER
+#define _UCRT_NOISY_NAN
+#endif
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
